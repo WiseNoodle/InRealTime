e2a9cc99341ea3a17b35e82882cbb5e80f1286d3
WiseNoodle <wisenoodle21@gmail>
2023-07-07 17:03:09 -0400

    Initial commit

---
+++ src/main/java/net/wisenoodle/inrealtime/InRealTime.java
@@ -0,0 +1,16 @@
+package net.wisenoodle.inrealtime;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class InRealTime implements ModInitializer {
+	public static boolean timeSyncToggle = true;
+	public static final String MOD_ID = "inrealtime";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("In Real-Time 1.0.0 Loaded Successfully!");
+	}
+}
---
+++ src/main/java/net/wisenoodle/inrealtime/mixin/ExampleMixin.java
@@ -0,0 +1,40 @@
+package net.wisenoodle.inrealtime.mixin;
+
+import net.minecraft.SharedConstants;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.level.ServerWorldProperties;
+import net.wisenoodle.inrealtime.InRealTime;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+
+import java.time.LocalTime;
+
+// HUGE thanks and credit to:
+// Code Hammering Specialist
+// Project Unfinisher
+// Koshka
+// For helping me learn how to make a mod (they carried me)
+
+
+@Mixin(ServerWorld.class)
+public abstract class ExampleMixin {
+	@Shadow
+	@Final
+	private ServerWorldProperties worldProperties;
+	private static final double CONSTANT_OF_MULTIPLICATION = (double)SharedConstants.TICKS_PER_SECOND/72d;
+	@Inject(method = "setTimeOfDay(J)V", at = @At(value = "HEAD"), cancellable = true)
+	public void setTimeOfDayCallback(long timeOfDay, CallbackInfo ci) {
+		long equivalentTime = Math.round(((LocalTime.now().toSecondOfDay() * CONSTANT_OF_MULTIPLICATION) - 6000));
+		if (equivalentTime < 0) equivalentTime = SharedConstants.TICKS_PER_IN_GAME_DAY + equivalentTime ;
+		if(InRealTime.timeSyncToggle == true) {
+			this.worldProperties.setTimeOfDay(equivalentTime);
+		}
+		ci.cancel();
+	}
+}
+
