d76e428b3c33df57d8eeee352146030af5d1997e
WiseNoodle <wisenoodle21@gmail>
2023-07-07 17:11:43 -0400

    Updated mixin filenames

--- src/main/java/net/wisenoodle/inrealtime/mixin/MainMixin.java
+++ src/main/java/net/wisenoodle/inrealtime/mixin/MainMixin.java
@@ -7,6 +7,7 @@ import net.wisenoodle.inrealtime.InRealTime;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,12 +27,13 @@ public abstract class MainMixin {
 	@Shadow
 	@Final
 	private ServerWorldProperties worldProperties;
+	@Unique
 	private static final double CONSTANT_OF_MULTIPLICATION = (double)SharedConstants.TICKS_PER_SECOND/72d;
 	@Inject(method = "setTimeOfDay(J)V", at = @At(value = "HEAD"), cancellable = true)
 	public void setTimeOfDayCallback(long timeOfDay, CallbackInfo ci) {
 		long equivalentTime = Math.round(((LocalTime.now().toSecondOfDay() * CONSTANT_OF_MULTIPLICATION) - 6000));
 		if (equivalentTime < 0) equivalentTime = SharedConstants.TICKS_PER_IN_GAME_DAY + equivalentTime ;
-		if(InRealTime.timeSyncToggle == true) {
+		if(InRealTime.timeSyncToggle) {
 			this.worldProperties.setTimeOfDay(equivalentTime);
 		}
 		ci.cancel();
